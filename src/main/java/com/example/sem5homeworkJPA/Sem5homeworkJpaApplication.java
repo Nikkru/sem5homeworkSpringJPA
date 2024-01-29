f9b2e164f01cf9402d2ddd02a2fd713a28f9e301
NikNik <56563372+Nikkru@users.noreply.github.com>
2024-01-29 14:41:40 +0300

    init

---
+++ src/main/java/com/example/sem5homeworkJPA/Sem5homeworkJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sem5homeworkJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem5homeworkJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem5homeworkJpaApplication.class, args);
+	}
+
+}
